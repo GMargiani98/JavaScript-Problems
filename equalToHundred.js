a0a701b55d15b2d96d022a155eb66f7cb268b697
george <g.margiani98@gmail.com>
2022-12-29 14:47:05 +0400

    two

---
+++ equalToHundred.js
@@ -0,0 +1,3 @@
+// Write a JS program to check two numbers and return true if one of the number is 100 or if the sum of the two numbers is 100
+
+const equalTo100 = (a, b) => a === 100 || b === 100 || a + b === 100;
---
+++ extensionName.js
@@ -0,0 +1,4 @@
+// write a JS program to get the extension of a filename
+
+const getFileExtension = (fileName) =>
+  fileName.slice(fileName.lastIndexOf('.'));
